376b89d1eefdfca66b49e2090b476c94a3967a5d
Diego <diego_sousa.s@outlook.com>
2023-06-24 16:09:26 -0300

    Criando classe Contact

---
+++ DesktopContactsApp/Domain/Contact.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+
+namespace DesktopContactsApp.Domain
+{
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; private set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(50)]
+        [Unique]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
