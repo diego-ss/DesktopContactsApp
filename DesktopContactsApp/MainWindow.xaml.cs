438bcbce9b2af0b986705ca5432776fb74d1dce5
Diego <diego_sousa.s@outlook.com>
2023-06-24 15:15:22 -0300

    Adicionar arquivos de projeto.

---
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+        }
+    }
+}
---
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Lógica interna para NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Save contact
+            Close();
+        }
+    }
+}
